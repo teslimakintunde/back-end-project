9c24d5ac51db29c505c0c2feea968bbb9ce4648a
Akintunde Teslim Akinmuyiwa <akinmuyiwateslim@gmail.com>
2025-04-20 07:34:38 +0100

    modifying package json for vercel

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,23 +1,23 @@
-// import allowedOrigin from "./allowedOrigins.js";
+import allowedOrigin from "./allowedOrigins.js";
 
-// const corsOptions = {
-//   origin: (origin, callback) => {
-//     if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
-//       callback(null, true);
-//     } else {
-//       callback(new Error("Not allowed cors"));
-//     }
-//   },
-//   optionsSuccessStatus: 200,
-// };
-
-// export default corsOptions;
 const corsOptions = {
-  origin: [
-    "https://back-end-project.vercel.app", // Replace with your Vercel URL
-    "http://localhost:3500",
-  ],
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
+  origin: (origin, callback) => {
+    if (allowedOrigin.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed cors"));
+    }
+  },
+  optionsSuccessStatus: 200,
 };
+
 export default corsOptions;
+// const corsOptions = {
+//   origin: [
+//     "https://back-end-project.vercel.app", // Replace with your Vercel URL
+//     "http://localhost:3500",
+//   ],
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+// };
+// export default corsOptions;
